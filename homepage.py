0645f58483e806b51344518e2a719d8b7b7c1774
Bijal Patel <bjp12@berkeley.edu>
2019-06-13 11:20:23 -0700

    Add files via upload

---
+++ homepage.py
@@ -0,0 +1,14 @@
+import json
+from flask import Flask
+from flask import render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5004, debug=True)
+
