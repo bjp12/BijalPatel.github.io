fd323bb1bc88c686bad5f6da5c9211c6d9b01db4
bjp12 <bjp12@berkeley.edu>
2019-10-09 12:27:58 -0700

    removed comments

--- freeze.py
+++ freeze.py
@@ -3,26 +3,6 @@ from website import app
 
 freezer = Freezer(app)
 
-# @freezer.register_generator
-# def home():
-# 	yield '/home'
-
-# @freezer.register_generator
-# def projects():
-# 	yield '/projects'
-
-# @freezer.register_generator
-# def photography():
-# 	yield '/photography'
-
-# @freezer.register_generator
-# def teaching():
-# 	yield '/teaching'
-
-# @freezer.register_generator
-# def traveling():
-# 	yield '/traveling'
-
 if __name__ == '__main__':
 	freezer.run(debug=True)
 
