fca08de0e41c15ee4b0605fb09c15f6b0b3d3be5
bjp12 <bjp12@berkeley.edu>
2019-10-09 12:53:35 -0700

    flatpages

--- website/app.py
+++ website/app.py
@@ -1,15 +1,25 @@
-import json
-from flask import Flask
-from flask import render_template, request
+from flask import Flask, render_template, request
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
 
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
+pages = FlatPages(app)
+freezer = Freezer(app)
 
 @app.route('/')
-
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/<path:path>/')
+def page(path):
+	page = pages.get_or_404(path)
+	return render_template('page.html', page=page)
 
 @app.route('/projects')
 def projects():
